2e093e099d1274545ec54c233eade771d75f87d6
nickaisbitt <nickaisbitt@hotmail.com>
2026-09-12 02:27:32 -0400

    Deploy Balloon Burst 2.1.0 (from modern-tower-defense-app@6767ab7)

--- sw.js
+++ sw.js
@@ -19,10 +19,10 @@
 // Registered web-only (never inside the native WKWebView) and only in
 // production; src/serviceWorker.ts guards both.
 
-const VERSION = "2.1.0+b5e3a8bf";
+const VERSION = "2.1.0+2c97c36b";
 const CACHE = `balloon-burst-v${VERSION}`;
 
-const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./privacy.html","./icon-180.png","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./assets/fredoka-var-DOQG_Jwn.woff2","./assets/index-BP5lC6PI.css","./assets/index-CogXA_NK.js","./assets/index-tshBoB4U.js","./assets/nunito-var-CjueodBP.woff2","./assets/web-B2QkyWOU.js"];
+const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./privacy.html","./icon-180.png","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./assets/fredoka-var-DOQG_Jwn.woff2","./assets/index-BP5lC6PI.css","./assets/index-CtrjKKGb.js","./assets/index-DIHjv8Ua.js","./assets/nunito-var-CjueodBP.woff2","./assets/web-C409Fp0d.js"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
